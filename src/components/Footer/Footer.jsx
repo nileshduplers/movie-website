8e1071cac10d1ffdde18d6cbea69bcc1f76b0b97
nileshduplers <155451010+nileshduplers@users.noreply.github.com>
2024-01-25 12:48:09 +0530

    updated pagination

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="py-10">
-      <div className="container">
+      <div className="container m-auto">
         <div className="grid">
           <p className="text-center text-xl text-white">
             &copy; Copyright Since 2024 under my.com
--- src/components/StreamMovie/StreamMovie.jsx
+++ src/components/StreamMovie/StreamMovie.jsx
@@ -1,50 +1,74 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Pagination from "../usePagination";
 
 function StreamMovie() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsPerPage] = useState(10);
+
   useEffect(() => {
     axios
-      .get(
-        "https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies"
-      )
+      .get("https://jsonplaceholder.typicode.com/photos")
       .then((response) => {
         setData(response.data);
       });
   }, []);
 
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  const currentRecords = data.slice(indexOfFirstRecord, indexOfLastRecord);
+  const nPages = Math.ceil(data.length / recordsPerPage);
+
   return (
     <section className="stream-movie py-10">
-      <div className="container m-auto">
+      <div className="container m-auto px-5">
         <h1 className="text-center text-5xl text-white">
           Stream Latest Movies Now!
         </h1>
-
-        <div className="grid grid-cols-4 gap-4 py-10">
-          {data.map((data) => {
+        <div className="filter-data py-10">
+          <ul className="flex items-center justify-center text-3xl">
+            <li className="mx-5"></li>
+          </ul>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 py-10">
+          {currentRecords.map((data) => {
             return (
               <div
-                key={data.Title}
+                key={data.id}
                 className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
               >
                 <a href="#">
                   <img
                     className="rounded-t-lg w-full"
-                    src={data.Poster}
+                    src={data.thumbnailUrl}
                     alt=""
                   />
                 </a>
                 <div className="p-5">
                   <a href="#">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      {data.Title}
+                      {data.title}
                     </h5>
                   </a>
+                </div>
+                <div className="year p-5 py-2">
+                  <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {data.Year}
+                  </h6>
                 </div>
               </div>
             );
           })}
         </div>
+        <Pagination
+          nPages={nPages}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+        ;
       </div>
     </section>
   );
